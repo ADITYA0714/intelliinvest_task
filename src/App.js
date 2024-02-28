245452123a45eadb5992a850d8bf4bb374f38e82
Adi7ya <95866052+ADITYA0714@users.noreply.github.com>
2024-02-28 09:36:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ import Banner from './components/banner/Banner'
 import Products from './components/products/Products';
 import Footer from './components/footer/Footer';
 import Availability from './components/availability/Availability';
-// import Home from './components/home/Home';
-// import About from './components/about/About';
-// import Skills from './components/skills/Skills';
-// import Services from './components/services/Services';
-// import Qualification from './components/Qualification/Qualification';
-// import Contact from './components/contact/Contact';
 
 function App() {
   return (
